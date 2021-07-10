b926dd821a0b396bce6f622920aba878b38b4b1e
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-07-10 15:35:16 -0300

    feat(mobile): add global themes
    Signed-off-by: Hállan da Silva Costa <hallan.costa1@hotmail.com>

---
+++ mobile/src/global/styles/theme.ts
@@ -0,0 +1,14 @@
+export const theme = {
+  colors: {
+    primaryOng: '#030818',
+    secondaryOng: '#030818',
+    tertiaryOng: '#04D361',
+    quaternaryOng: '#02006F'
+  },
+
+  fonts: {
+    title700: 'Roboto_700Bold',
+    title600: 'Poppins_600SemiBold',
+    text400: 'Poppins_400Regular'
+  }
+}
