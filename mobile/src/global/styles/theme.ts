538060f28b689c5c6ba026f7e4806d6af040d141
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-07-13 11:34:59 -0300

    feat(mobile): add color primaryOng100

--- mobile/src/global/styles/theme.ts
+++ mobile/src/global/styles/theme.ts
@@ -1,14 +1,18 @@
 export const theme = {
   colors: {
-    primaryOng: '#030818',
-    secondaryOng: '#030818',
+    primaryOng100: '#030818',
+    primaryOng90: '#0A1033',
+    primaryOng80: '#030848',
+
+    secondaryOng: '#4c4c4c',
     tertiaryOng: '#04D361',
     quaternaryOng: '#02006F'
   },
 
   fonts: {
-    title700: 'Roboto_700Bold',
-    title600: 'Poppins_600SemiBold',
+    title900: 'Archivo_900Black',
+    title700: 'Poppins_700Bold',
+    title500: 'Poppins_500Medium',
     text400: 'Poppins_400Regular'
   }
 }
