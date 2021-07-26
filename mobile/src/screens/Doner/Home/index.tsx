8ef421af55f9e7c59b6fa7a1ca984eb2072d0a42
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-07-26 02:00:08 -0300

    feat(mobile): add screen Home
    Signed-off-by: Hállan da Silva Costa <hallan.costa1@hotmail.com>

---
+++ mobile/src/screens/Doner/Home/index.tsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import { useNavigation } from '@react-navigation/native';
+
+import { Background } from '../../../components/Background';
+import { Header } from '../../../components/Header';
+import { ButtonBig } from '../../../components/ButtonBig';
+import { Presentation } from '../../../components/Presentation';
+import { ButtonLogout } from '../../../components/ButtonLogout';
+
+import { theme } from '../../../global/styles/theme';
+import { 
+  styles,
+  Container,
+  Content
+} from './styles';
+
+export function Home(){
+  const { navigate } = useNavigation();
+
+  function handleNavigateToListIncident() {
+    // navigate('MyIncidents');
+    alert('handleNavigateToListIncident');
+  }
+
+  return (
+    <Background gradient="donor">
+      <Header 
+        right={<ButtonLogout gradient="donor" />}
+      />
+
+      <Presentation
+        title={'Olá Hállan. \nSeja bem vindo'}
+        subtitle={'Ajude os incidentes sendo \num doador fiel.'}
+      />
+   
+      <Content>
+        {/* 
+        <ButtonBig 
+          title="" 
+          color='' 
+        /> 
+        */}
+
+        <ButtonBig 
+          title={'Listar\n Incidente'} 
+          color={theme.colors.green}
+          onPress={handleNavigateToListIncident}
+        />
+      </Content>
+    </Background>
+  );
+}
---
+++ mobile/src/screens/Doner/Home/styles.ts
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
+
+export const Container = styled.View`
+  flex: 1;
+`;
+
+export const Content = styled.View`
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+`;
