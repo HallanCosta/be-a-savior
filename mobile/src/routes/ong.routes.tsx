ab1bcf41788daa2ad97191eba53f4f222fcb6ac1
Hállan da Silva Costa <hallan.costa1@hotmail.com>
2021-07-26 01:53:12 -0300

    feat(mobile): add routes Ong
    Signed-off-by: Hállan da Silva Costa <hallan.costa1@hotmail.com>

---
+++ mobile/src/routes/ong.routes.tsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import { createStackNavigator  } from '@react-navigation/stack';
+
+import { Home } from '../screens/Ong/Home';
+import { CreateIncident } from '../screens/Ong/CreateIncident';
+import { MyIncidents } from '../screens/Ong/MyIncidents';
+import { EditIncident } from '../screens/Ong/EditIncident';
+import { DetailsDonor } from '../screens/Ong/DetailsDonor';
+
+import { theme } from '../global/styles/theme';
+
+export function OngRoutes(){
+  const { Navigator, Screen } = createStackNavigator();
+  return (
+    <Navigator 
+      headerMode="none"
+      screenOptions={{ 
+        cardStyle: {
+          backgroundColor: theme.colors.ong.background100
+        } 
+      }}
+    >
+      <Screen 
+        name="Home"
+        component={Home}
+      />
+      <Screen 
+        name="CreateIncident"
+        component={CreateIncident}
+      />
+      <Screen 
+        name="MyIncidents"
+        component={MyIncidents}
+      />
+      <Screen 
+        name="EditIncident"
+        component={EditIncident}
+      />
+      <Screen 
+        name="DetailsDonor"
+        component={DetailsDonor}
+      />
+    </Navigator>
+  );
+}
