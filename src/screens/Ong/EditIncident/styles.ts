a7b358967596fe8c801dbf699ff3b430fa4ddab4
HallanCosta <hallan.costa1@hotmail.com>
2022-07-13 12:16:25 -0300

    feat: add ux donations accumulated screen edit incident

--- src/screens/Ong/EditIncident/styles.ts
+++ src/screens/Ong/EditIncident/styles.ts
@@ -1,6 +1,8 @@
 import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
+import { theme } from '../../../global/styles/theme';
+
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -20,13 +22,28 @@ export const Form = styled.View`
   border-radius: 20px;
 `;
 
+export const DonationAccumulateds = styled.Text`
+  font-size: 20px;
+  color: #ffffff;
+  font-family: ${theme.fonts.text400};
+  padding-left: 30px;
+  margin-top: 10px;
+`;
+
+export const DonationAccumulatedsAmount = styled.Text`
+  font-size: 20px;
+  color: #f1f1f1;
+  font-family: ${theme.fonts.title700};
+  padding-left: 30px;
+`;
+
 export const Footer = styled.View`
   width: 100%;
   flex-direction: row;
   align-items: center;
   justify-content: space-around;
 
-  margin-top: 20px;
+  margin-top: 15px;
   margin-bottom: 45px;
 
   padding-left: 30px;
