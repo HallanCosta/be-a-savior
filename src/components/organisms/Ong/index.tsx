93a835dc5bd403129424a6ee343349ffb45ea5bc
HallanCosta <hallan.costa1@hotmail.com>
2022-08-03 02:38:03 -0300

    chore: remove styled-components DetailsOng

--- src/components/organisms/Ong/index.tsx
+++ src/components/organisms/Ong/index.tsx
@@ -1,14 +1,6 @@
 import React, { useEffect, useState, useCallback } from 'react';
-import { Alert } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
 
-import { Load } from '../../atoms/Load';
 import { InputCard } from '../../molecules/InputCard';
-
-import { api } from '../../../services/api';
-
-import { useAuth } from '../../../hooks/auth';
-import { useIncident } from '../../../hooks/incident';
 
 import { theme } from '../../../global/styles/theme';
 import { 
@@ -32,43 +24,22 @@ type Props = {
 export function Ong({
   data,
 }: Props){
-
-  const { user } = useAuth();
-
-  const { navigate } = useNavigation();
-
-  const [loading, setLoading] = useState(false);
-
-  function handleNavigateToEditIncident() {
-
-  }
-
   return (
-    <>
-      {
-        loading
-        ?
-        <Load />
-        :
-        <Container>
-          <InputCard 
-            title="Nome da Ong"
-            subtitle={data.name}
-          />
+    <Container>
+      <InputCard 
+        title="Nome da Ong"
+        subtitle={data.name}
+      />
 
-          <InputCard 
-            title="Email"
-            subtitle={data.email}
-          />
+      <InputCard 
+        title="Email"
+        subtitle={data.email}
+      />
 
-          <InputCard 
-            title="Telefone"
-            subtitle={String(data.phone)}
-          />
-
-
-        </Container>
-      }
-    </>
+      <InputCard 
+        title="Telefone"
+        subtitle={String(data.phone)}
+      />
+    </Container>
   );
 }
--- src/screens/Donor/DetailsOng/styles.ts
+++ src/screens/Donor/DetailsOng/styles.ts
@@ -10,23 +10,3 @@ export const styles = StyleSheet.create({
 export const Container = styled.View`
   flex: 1;
 `;
-
-export const Footer = styled.View`
-  width: 100%;
-  flex-direction: column;
-  justify-content: space-between;
-
-  margin-top: 50px;
-  margin-bottom: 52px;
-
-  padding: 0 30px 0 30px;
-`;
-
-export const ButtonWrapper = styled.View`
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  background-color: #ffffff;
-  border-radius: 20px;
-  padding: 30px;
-`;
