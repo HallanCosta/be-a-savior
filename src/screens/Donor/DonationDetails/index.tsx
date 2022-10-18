218b6feb1bac84f81a1fd2174efeb253595f767c
HallanCosta <hallan.costa1@hotmail.com>
2022-10-17 21:28:04 -0300

    removal requests that are unnecessary

--- src/screens/Donor/DonationDetails/index.tsx
+++ src/screens/Donor/DonationDetails/index.tsx
@@ -31,18 +31,14 @@ type RouteParams = {
 };
 
 export function DonationDetails() {
-  const { user } = useAuth();
-
   const { params } = useRoute();
   const routeParams = params as RouteParams;
 
   const [incident, setIncident] = useState<IncidentProps>({} as IncidentProps);
-  const [incidents, setIncidents] = useState<IncidentProps[]>([]);
   const [ong, setOng] = useState<UserResponse>({} as UserResponse);
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    console.log(routeParams.incident.user_id);
     load();
   }, []);
 
@@ -50,50 +46,24 @@ export function DonationDetails() {
     setLoading(true);
 
     try {
-      const response1 = await fetchUser(routeParams.incident.user_id);
+      const responseOng = await fetchUser(routeParams.incident.user_id);
 
-      if (!response1) {
+      if (!responseOng) {
         Alert.alert("Ops", "Não foi possível a ong");
         setLoading(false);
         return;
       }
 
-      const response = await loadIncidents({});
+      const responseIncident = await loadIncident(routeParams.incident.id);
 
-      if (!response) {
-        Alert.alert(
-          "Ops",
-          "Não foi possível buscar os incidentes que não foram doados"
-        );
-        setLoading(false);
-        return;
-      }
-
-      const response3 = await loadIncidents({
-        ongId: user.id,
-        donated: "incomplete",
-      });
-
-      if (!response3) {
-        Alert.alert(
-          "Ops",
-          "Não foi possível buscar os incidentes que estão com as doações incompletas"
-        );
-        setLoading(false);
-        return;
-      }
-
-      const response4 = await loadIncident(routeParams.incident.id);
-
-      if (!response4) {
+      if (!responseIncident) {
         Alert.alert("Ops", "Não foi possível buscar as doações do incidente");
         setLoading(false);
         return;
       }
 
-      setOng(response1);
-      setIncidents(response.incidents);
-      setIncident(response4);
+      setOng(responseOng);
+      setIncident(responseIncident);
       setLoading(false);
     } catch (err) {
       console.log("Error: ", err);
